ae4bc58d095828310a8a3e0017a3c5ae3a7d91a8
rjleyy <rileylarsen19@gmail.com>
2024-09-24 14:50:40 -0400

    Reformatted file structure

--- specs/base_test.py
+++ specs/base_test.py
@@ -8,8 +8,8 @@ class BaseTest(BaseCase):
 
     def update_bs_status(self, status):
         session_id = self.driver.session_id
-        bs_username = os.environ.get('BROWSERSTACK_USERNAME')
-        bs_access_key = os.environ.get('BROWSERSTACK_ACCESS_KEY')
+        bs_username = os.environ.get("BROWSERSTACK_USERNAME")
+        bs_access_key = os.environ.get("BROWSERSTACK_ACCESS_KEY")
         auth_credentials = (bs_username, bs_access_key)
         url = f"{BROWSERSTACK_BASE_URL}/automate/sessions/{session_id}.json"
         payload = {"status": status}
